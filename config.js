d9c6d8659f76dff836f173f5dea38e76619b0529
papi zen <zentot015@gmail.com>
2026-09-22 15:06:36 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,40 @@
+// CONFIG BOT WA STORE - © ZenDlouis
+// GANTI DATA DI SINI SESUAI TOKO LU!
+
+module.exports = {
+    // ===== INFO BOT =====
+    botName: "Store Bot V1",
+    ownerName: "𝐙𝐄𝐍 𝐒𝐓𝐎𝐑𝐄",
+    ownerNumber: "6282122997137", // Ganti nomor owner (format 62xxx)
+    
+    // ===== INFO TOKO =====
+    storeName: "🛒 ZEN STORE 🛒",
+    storeDesc: "Toko Online Terpercaya 24/7",
+    storeAddress: "Jakarta, Indonesia",
+    storeHours: "24 Jam Online",
+    
+    // ===== PEMBAYARAN =====
+    payment: {
+        dana: "082130326651", 
+        gopay: "082162307217",
+        qris: "https://files.catbox.moe/cnjka6.png",
+    },
+    
+    // ===== FITUR =====
+    autoRead: true,        // Auto baca pesan
+    autoTyping: true,      // Auto typing
+    antiSpam: true,        // Anti spam
+    welcomeMsg: true,      // Welcome message
+    
+    // ===== PREFIX =====
+    prefix: "!",
+    
+    // ===== ADMIN =====
+    adminNumbers: [
+        "6282122997137",    // Admin 1
+        "6285381111956"     // Admin 2
+    ],
+    
+    // ===== API =====
+    apiKey: "your-api-key-here"
+};
